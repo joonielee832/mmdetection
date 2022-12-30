8fc38b28e57a1358b4ccf0a5a1b6227867b4c5ae
joonielee832 <john.lee@robotics.utias.utoronto.ca>
2022-12-29 22:55:54 -0500

    Add more probabilistic_retinanet implementation;

--- mmdet/models/dense_heads/prob_retina_head.py
+++ mmdet/models/dense_heads/prob_retina_head.py
@@ -1,12 +1,21 @@
+import warnings
+
+import torch
 import torch.nn as nn
+from mmcv.runner import force_fp32
 from mmcv.cnn import ConvModule
 
-from ..builder import HEADS
-from .anchor_head import AnchorHead
-
+from mmdet.core import (anchor_inside_flags, build_assigner, build_bbox_coder,
+                        build_prior_generator, build_sampler, images_to_levels,
+                        multi_apply, unmap)
+from ..builder import HEADS, build_loss
+from .base_dense_head import BaseDenseHead
+# from .anchor_head import AnchorHead
+from .retina_head import RetinaHead
+from .dense_test_mixins import BBoxTestMixin
 
 @HEADS.register_module()
-class ProbabilisticRetinaHead(AnchorHead):
+class ProbabilisticRetinaHead(RetinaHead):
     r"""An anchor-based head used in `RetinaNet
     <https://arxiv.org/pdf/1708.02002.pdf>`_.
 
@@ -69,9 +78,13 @@ class ProbabilisticRetinaHead(AnchorHead):
                 # Number of elements required to describe an NxN covariance matrix is
                 # computed as: (N*(N+1))/2
                 self.bbox_cov_dims = 10
+
         super(ProbabilisticRetinaHead, self).__init__(
             num_classes,
             in_channels,
+            stacked_convs=stacked_convs,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg,
             anchor_generator=anchor_generator,
             init_cfg=init_cfg,
             **kwargs)
@@ -161,12 +174,6 @@ class ProbabilisticRetinaHead(AnchorHead):
                 bbox_pred (Tensor): Box energies / deltas for a single scale
                     level, the channels number is num_anchors * 4.
         """
-        # cls_feat = x
-        # reg_feat = x
-        # for cls_conv in self.cls_convs:
-        #     cls_feat = cls_conv(cls_feat)
-        # for reg_conv in self.reg_convs:
-        #     reg_feat = reg_conv(reg_feat)
         cls_feat = self.cls_net(x)
         reg_feat = self.reg_net(x)
         cls_score = self.retina_cls(cls_feat)
@@ -183,3 +190,166 @@ class ProbabilisticRetinaHead(AnchorHead):
             bbox_cov = None
         return cls_score, bbox_pred, cls_var, bbox_cov
         # return cls_score, bbox_pred
+
+    def forward(self, feats):
+        """Forward features from the upstream network.
+
+        Args:
+            feats (tuple[Tensor]): Features from the upstream network, each is
+                a 4D-tensor.
+
+        Returns:
+            tuple: A tuple of classification scores and bbox prediction.
+
+                - cls_scores (list[Tensor]): Classification scores for all \
+                    scale levels, each is a 4D-tensor, the channels number \
+                    is num_base_priors * num_classes.
+                - bbox_preds (list[Tensor]): Box energies / deltas for all \
+                    scale levels, each is a 4D-tensor, the channels number \
+                    is num_base_priors * 4.
+                - cls_vars (list[Tensor]): Classification variances for all \
+                    scale levels, each is a 4D-tensor, the channels number \
+                    is self.num_base_priors * num_classes.
+                - bbox_covs (list[Tensor]): Box covariances for all scale \
+                    levels, each is a 4-D tensor, the channels number is \
+                    num_base_priors * bbox_cov_dims (4)
+        """
+        return multi_apply(self.forward_single, feats)
+    
+    #TODO: loss_single function overwrite from anchor_head
+    def loss_single(self, cls_score, bbox_pred, cls_var, bbox_cov, anchors, labels, label_weights,
+                    bbox_targets, bbox_weights, num_total_samples):
+        """Compute loss of a single scale level.
+
+        Args:
+            cls_score (Tensor): Box scores for each scale level
+                Has shape (N, num_anchors * num_classes, H, W).
+            bbox_pred (Tensor): Box energies / deltas for each scale
+                level with shape (N, num_anchors * 4, H, W).
+            anchors (Tensor): Box reference for each scale level with shape
+                (N, num_total_anchors, 4).
+            labels (Tensor): Labels of each anchors with shape
+                (N, num_total_anchors).
+            label_weights (Tensor): Label weights of each anchor with shape
+                (N, num_total_anchors)
+            bbox_targets (Tensor): BBox regression targets of each anchor
+                weight shape (N, num_total_anchors, 4).
+            bbox_weights (Tensor): BBox regression loss weights of each anchor
+                with shape (N, num_total_anchors, 4).
+            num_total_samples (int): If sampling, num total samples equal to
+                the number of total anchors; Otherwise, it is the number of
+                positive anchors.
+
+        Returns:
+            dict[str, Tensor]: A dictionary of loss components.
+        """
+        # classification loss
+        labels = labels.reshape(-1)
+        label_weights = label_weights.reshape(-1)
+        cls_score = cls_score.permute(0, 2, 3,
+                                      1).reshape(-1, self.cls_out_channels)
+        loss_cls = self.loss_cls(
+            cls_score, labels, label_weights, avg_factor=num_total_samples)
+        # regression loss
+        bbox_targets = bbox_targets.reshape(-1, 4)
+        bbox_weights = bbox_weights.reshape(-1, 4)
+        bbox_pred = bbox_pred.permute(0, 2, 3, 1).reshape(-1, 4)
+        if self.reg_decoded_bbox:
+            # When the regression loss (e.g. `IouLoss`, `GIouLoss`)
+            # is applied directly on the decoded bounding boxes, it
+            # decodes the already encoded coordinates to absolute format.
+            anchors = anchors.reshape(-1, 4)
+            bbox_pred = self.bbox_coder.decode(anchors, bbox_pred)
+        loss_bbox = self.loss_bbox(
+            bbox_pred,
+            bbox_targets,
+            bbox_weights,
+            avg_factor=num_total_samples)
+        return loss_cls, loss_bbox, None, None
+    
+    #TODO: loss function overwrite from anchor_head
+    @force_fp32(apply_to=('cls_scores', 'bbox_preds', 'cls_vars', 'bbox_covs'))
+    def loss(self,
+             cls_scores,
+             bbox_preds,
+             cls_vars,
+             bbox_covs,
+             gt_bboxes,
+             gt_labels,
+             img_metas,
+             gt_bboxes_ignore=None):
+        """Compute losses of the head.
+
+        Args:
+            cls_scores (list[Tensor]): Box scores for each scale level
+                Has shape (N, num_anchors * num_classes, H, W)
+            bbox_preds (list[Tensor]): Box energies / deltas for each scale
+                level with shape (N, num_anchors * 4, H, W)
+            cls_vars (list[Tensor]): Box score variances for each scale level
+                Has shape (N, num_anchors * num_classes, H, W)
+            bbox_covs (list[Tensor]): Box covariances for each scale
+                level with shape (N, num_anchors * 4, H, W)
+            gt_bboxes (list[Tensor]): Ground truth bboxes for each image with
+                shape (num_gts, 4) in [tl_x, tl_y, br_x, br_y] format.
+            gt_labels (list[Tensor]): class indices corresponding to each box
+            img_metas (list[dict]): Meta information of each image, e.g.,
+                image size, scaling factor, etc.
+            gt_bboxes_ignore (None | list[Tensor]): specify which bounding
+                boxes can be ignored when computing the loss. Default: None
+
+        Returns:
+            dict[str, Tensor]: A dictionary of loss components.
+        """
+        featmap_sizes = [featmap.size()[-2:] for featmap in cls_scores]
+        assert len(featmap_sizes) == self.prior_generator.num_levels
+
+        device = cls_scores[0].device
+
+        anchor_list, valid_flag_list = self.get_anchors(
+            featmap_sizes, img_metas, device=device)
+        label_channels = self.cls_out_channels if self.use_sigmoid_cls else 1
+        cls_reg_targets = self.get_targets(
+            anchor_list,
+            valid_flag_list,
+            gt_bboxes,
+            img_metas,
+            gt_bboxes_ignore_list=gt_bboxes_ignore,
+            gt_labels_list=gt_labels,
+            label_channels=label_channels)
+        if cls_reg_targets is None:
+            return None
+        (labels_list, label_weights_list, bbox_targets_list, bbox_weights_list,
+         num_total_pos, num_total_neg) = cls_reg_targets
+        num_total_samples = (
+            num_total_pos + num_total_neg if self.sampling else num_total_pos)
+
+        # anchor number of multi levels
+        num_level_anchors = [anchors.size(0) for anchors in anchor_list[0]]
+        # concat all level anchors and flags to a single tensor
+        concat_anchor_list = []
+        for i in range(len(anchor_list)):
+            concat_anchor_list.append(torch.cat(anchor_list[i]))
+        all_anchor_list = images_to_levels(concat_anchor_list,
+                                           num_level_anchors)
+
+        losses_cls, losses_bbox, losses_cls_vars, losses_bbox_covs = multi_apply(
+            self.loss_single,
+            cls_scores,
+            bbox_preds,
+            cls_vars,
+            bbox_covs,
+            all_anchor_list,
+            labels_list,
+            label_weights_list,
+            bbox_targets_list,
+            bbox_weights_list,
+            num_total_samples=num_total_samples)
+        
+        #! Remove
+        losses_cls_vars = [torch.ones_like(l_cls) for l_cls in losses_cls]
+        losses_bbox_covs = [torch.ones_like(l_bbox) for l_bbox in losses_bbox]
+        
+        return dict(loss_cls_score=losses_cls, 
+                    loss_bbox_reg=losses_bbox, 
+                    loss_cls_var=losses_cls_vars, 
+                    loss_bbox_cov=losses_bbox_covs)
--- mmdet/models/detectors/probabilistic_retinanet.py
+++ mmdet/models/detectors/probabilistic_retinanet.py
@@ -1,10 +1,16 @@
-from ..builder import DETECTORS
+import warnings
 
-from .single_stage import SingleStageDetector
+import torch
+
+from mmdet.core import bbox2result
+from ..builder import DETECTORS, build_backbone, build_head, build_neck
+
+# from .single_stage import SingleStageDetector
+from .retinanet import RetinaNet
 
 
 @DETECTORS.register_module()
-class ProbabilisticRetinaNet(SingleStageDetector):
+class ProbabilisticRetinaNet(RetinaNet):
 
     def __init__(self,
                  backbone,
@@ -16,3 +22,61 @@ class ProbabilisticRetinaNet(SingleStageDetector):
                  init_cfg=None):
         super(ProbabilisticRetinaNet, self).__init__(backbone, neck, bbox_head, train_cfg,
                                         test_cfg, pretrained, init_cfg)
+    
+    #TODO: simple_test function overwrite from single_stage
+    def simple_test(self, img, img_metas, rescale=False):
+        """Test function without test-time augmentation.
+
+        Args:
+            img (torch.Tensor): Images with shape (N, C, H, W).
+            img_metas (list[dict]): List of image information.
+            rescale (bool, optional): Whether to rescale the results.
+                Defaults to False.
+
+        Returns:
+            list[list[np.ndarray]]: BBox results of each image and classes.
+                The outer list corresponds to each image. The inner list
+                corresponds to each class.
+        """
+        breakpoint()
+        feat = self.extract_feat(img)
+        results_list = self.bbox_head.simple_test(
+            feat, img_metas, rescale=rescale)
+        bbox_results = [
+            bbox2result(det_bboxes, det_labels, self.bbox_head.num_classes)
+            for det_bboxes, det_labels in results_list
+        ]
+        return bbox_results
+
+    #TODO: aug_test function overwrite from single_stage
+    def aug_test(self, imgs, img_metas, rescale=False):
+        """Test function with test time augmentation.
+
+        Args:
+            imgs (list[Tensor]): the outer list indicates test-time
+                augmentations and inner Tensor should have a shape NxCxHxW,
+                which contains all images in the batch.
+            img_metas (list[list[dict]]): the outer list indicates test-time
+                augs (multiscale, flip, etc.) and the inner list indicates
+                images in a batch. each dict has image information.
+            rescale (bool, optional): Whether to rescale the results.
+                Defaults to False.
+
+        Returns:
+            list[list[np.ndarray]]: BBox results of each image and classes.
+                The outer list corresponds to each image. The inner list
+                corresponds to each class.
+        """
+        breakpoint()
+        assert hasattr(self.bbox_head, 'aug_test'), \
+            f'{self.bbox_head.__class__.__name__}' \
+            ' does not support test-time augmentation'
+
+        feats = self.extract_feats(imgs)
+        results_list = self.bbox_head.aug_test(
+            feats, img_metas, rescale=rescale)
+        bbox_results = [
+            bbox2result(det_bboxes, det_labels, self.bbox_head.num_classes)
+            for det_bboxes, det_labels in results_list
+        ]
+        return bbox_results
